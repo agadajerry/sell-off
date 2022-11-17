a79b3a8dd00e55605ecdc96f5df10f17c33a8093
Idoko Agada Jerry <Idokoidoko4@gmail.com>
2022-11-17 17:05:04 +0100

    naviagtion

--- App.tsx
+++ App.tsx
@@ -1,27 +1,61 @@
 // import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
-import { StyleSheet } from "react-native";
+
 import Screen from "./app/components/Screen";
-import ImageInputList from "./app/components/ImageInputList";
-import ListingEditingScreen from "./app/screen/ListingEditingScreen";
+import { Button, Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+
+const Stack = createNativeStackNavigator();
+
+const Tweet = ({ navigation }: any) => {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+      <Button
+        title="View Tweet"
+        onPress={() => navigation.navigate("TweetDetails", { id: 2 })}
+      />
+    </Screen>
+  );
+};
+
+const TweetDetails = ({ route }: any) => {
+  return (
+    <Screen>
+      <Text>Tweet Details {route.params.id}</Text>
+    </Screen>
+  );
+};
+
+const StackNavigator = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: "dodgerblue",
+      },
+      headerTintColor: "white",
+    }}
+  >
+    <Stack.Screen name="Tweet" component={Tweet} />
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+      options={{
+        headerStyle: {
+          backgroundColor: "tomato",
+        },
+        headerTintColor: "#fff",
+        headerShown: true,
+      }}
+    />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  const [imageUris, setImageUris] = React.useState<any[]>([]);
-
-  const handleAddImage = (uri: any) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemoveImage = (uri: any) => {
-    setImageUris(imageUris.filter((imageUri: any) => imageUri !== uri));
-  };
-  return <ListingEditingScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: "50%",
-    height: 30,
-    backgroundColor: "red",
-  },
-});
