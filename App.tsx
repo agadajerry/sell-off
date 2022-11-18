c3f138e2fc46ce53c5cefaa3afae0bd5a47c05e5
Idoko Agada Jerry <Idokoidoko4@gmail.com>
2022-11-18 09:00:08 +0100

    tab navigation

--- App.tsx
+++ App.tsx
@@ -3,8 +3,10 @@
 import Screen from "./app/components/Screen";
 import { Button, Text } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
@@ -52,10 +54,50 @@ const StackNavigator = () => (
   </Stack.Navigator>
 );
 
+const Account = () => {
+  return (
+    <Screen>
+      <Text>Account</Text>
+    </Screen>
+  );
+};
+// const Tab = createBottomTabNavigator();
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={{
+      tabBarActiveBackgroundColor: "tomato",
+      tabBarActiveTintColor: "white",
+      tabBarInactiveTintColor: "back",
+      tabBarInactiveBackgroundColor: "#eee",
+    }}
+  >
+    <Tab.Screen
+      name="Tweet"
+      component={Tweet}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons name="home" size={size} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Account"
+      component={Account}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons name="account" size={size} color={color} />
+        ),
+      }}
+    />
+  </Tab.Navigator>
+);
+
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
